2298cf6ef14d0934435818aa907aceb27669119a
xuxa88 <myrellafernanda785@gmail.com>
2024-08-30 08:19:42 -0300

    Adicionando arquivo js e css

---
+++ style.css
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(600, 600);
+    background("white")
+  }
+  
+  function draw() {
+    stroke("purple");
+    fill("yellow");
+      rect(mouseX, mouseY, 20, 35);
+    }
+  
